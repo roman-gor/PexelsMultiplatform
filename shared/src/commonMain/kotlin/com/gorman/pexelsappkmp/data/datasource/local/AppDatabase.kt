7b1b81b88be2d3a5eebee721bcb1c54843353c22
Roman <romangorbachev2006@gmail.com>
2025-12-18 00:23:52 +0300

    Update

--- shared/src/commonMain/kotlin/com/gorman/pexelsappkmp/data/datasource/local/AppDatabase.kt
+++ shared/src/commonMain/kotlin/com/gorman/pexelsappkmp/data/datasource/local/AppDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.gorman.pexelsappkmp.data.models.BookmarkImage
 
-@Database(entities = [BookmarkImage::class], version = 2)
+@Database(entities = [BookmarkImage::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun bookmarkImageDao(): BookmarksImageDao
 }
